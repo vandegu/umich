756d83c3a3da4fd96ff413a6afe06d870abf2d2e
vandegu <avandegu@umich.edu>
2017-08-24 14:27:40 -0400

    Initial commit
    A small code to calculate an meshgrid of areas; used for weighting averages on a 1 latitude x 1 longitude spherical grid.

---
+++ earthgrid_1x1.py
@@ -0,0 +1,32 @@
+# earthgrid_1x1.py
+
+# Create rough area meshgrid for a latitude x longitude grid on the earth. Can be for any sphere, if R is specified.
+
+class Earth(object):
+    
+    def __init__(self,R=6371.0):
+        self.R = R # Radius of Earth (km): 6371.0 km
+        self.margins = self.latlon_grid_boundaries()
+        A = self.area_on_sphere(self.margins[:,0],self.margins[:,1],self.margins[:,2],self.margins[:,3])
+        self.A = np.reshape(A,(180,360))
+    
+    def latlon_grid_boundaries(self,):
+        '''This method produces the four boundary points that serve as input for the area on a sphere method.'''
+
+        margins = np.empty((360*180,4))
+        for x,lat in enumerate(np.arange(-90,90)):
+            for y,lon in enumerate(np.arange(0,360)):
+                margins[x*360+y,0] = lat
+                margins[x*360+y,1] = lat+1
+                margins[x*360+y,2] = lon
+                margins[x*360+y,3] = lon+1
+
+        return margins
+        
+    def area_on_sphere(self,lat1,lat2,lon1,lon2):
+        '''Returns the area on the Earth bounded by a set of lons and lats (entered in degrees).'''
+
+        lat1,lat2,lon1,lon2 = np.deg2rad((lat1,lat2,lon1,lon2)) # Convert the lats,lons into radians.
+        area = self.R**2*(lon2-lon1)*(np.sin(lat2)-np.sin(lat1))
+
+        return area
